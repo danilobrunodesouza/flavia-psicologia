9814a4ba8317ba8781e8a4509b81573c429a0536
Danilo Bruno de Souza <danilobrunodesouza@gmail.com>
2021-12-04 14:03:32 -0300

    testes

--- js/scripts.js
+++ js/scripts.js
@@ -12,16 +12,16 @@ const targets = document.querySelectorAll('[data-animation]')
 const animationClass = 'animate'
 
 function animeScroll(){
-    const resultToAnyDeviceSize = window.innerHeight * 0.85
-    const windowTop = window.scrollY + resultToAnyDeviceSize;
-    targets.forEach(element => {
-        if(windowTop >  element.offsetTop){
-            element.classList.add(animationClass)
-        } else {
-            element.classList.remove(animationClass)
+    // const resultToAnyDeviceSize = window.innerHeight * 0.85
+    // const windowTop = window.scrollY + resultToAnyDeviceSize;
+    // targets.forEach(element => {
+    //     if(windowTop >  element.offsetTop){
+    //         element.classList.add(animationClass)
+    //     } else {
+    //         element.classList.remove(animationClass)
 
-        }
-    })
+    //     }
+    // })
 }
 
 
